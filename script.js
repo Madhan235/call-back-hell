a33891a02cd01795505e50f3baf6a66017c17888
Madhan235 <117820919+Madhan235@users.noreply.github.com>
2022-12-27 18:38:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+var count_10 = ()=>{
+    var call_count = document.querySelector('.count');
+ call_count.textContent = '10';
+};
+// call_count.style.display = 'none';
+var count_9 = ()=>{
+    var call_count = document.querySelector('.count');
+    call_count.textContent = '9';
+}
+var count_8 = ()=>{
+    var call_count = document.querySelector('.count');
+    call_count.textContent = '8';
+}
+var count_7 = ()=>{
+    var call_count = document.querySelector('.count');
+    call_count.textContent = '7';
+}
+var count_6 = ()=>{
+    var call_count = document.querySelector('.count');
+    call_count.textContent = '6';
+}
+var count_5 = ()=>{
+    var call_count = document.querySelector('.count');
+    call_count.textContent = '5';
+}
+var count_4 = ()=>{
+    var call_count = document.querySelector('.count');
+    call_count.textContent = '4';
+}
+var count_3 = ()=>{
+    var call_count = document.querySelector('.count');
+    call_count.textContent = '3';
+}
+var count_2 = ()=>{
+    var call_count = document.querySelector('.count');
+    call_count.textContent = '2';
+}
+var count_1 = ()=>{
+    var call_count = document.querySelector('.count');
+    call_count.textContent = '1';
+}
+var final_text = ()=>{
+ var wish = document.querySelector('.count');
+wish.textContent = 'HAPPY INDEPENDENCE DAY ALL !'
+}
+setTimeout(count_10,1000)
+setTimeout(count_9,2000)
+setTimeout(count_8,3000)
+setTimeout(count_7,4000)
+setTimeout(count_6,5000)
+setTimeout(count_5,6000)
+setTimeout(count_4,7000)
+setTimeout(count_3,8000)
+setTimeout(count_2,9000)
+setTimeout(count_1,10000)
+setTimeout(final_text,11000)
